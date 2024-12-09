4142e8b49212f7ea513c433ed26e6dd360723cbd
tatianakrasikova <krasikovatatana535@gmail.com>
2024-12-09 21:14:12 +0100

    commit2

---
+++ src/main/java/ait/cochort49/shop/homeworkshop/HomeworkshopApplication.java
@@ -0,0 +1,13 @@
+package ait.cochort49.shop.homeworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ait/cochort49/shop/homeworkshop/HomeworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.cochort49.shop.homeworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
